e4a0e82ced75c0e61f640218252b49b96e001289
dipak sarmah <sarmahd60@gmail.com>
2024-03-14 15:14:28 +0530

    Added customer review section and footer section.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,13 +4,14 @@ const Button = ({
   backgroundColor,
   borderColor,
   textColor,
+  fullWidth,
 }) => {
   return (
     <button
       className={`flex justify-center items-center gap-2 px-7 py-4  border rounded-full font-montserrat text-lg leading-none border-coral-red${
         backgroundColor
           ? ` ${textColor} ${backgroundColor} ${borderColor} `
-          : ' bg-coral-red  text-white border-coral-red'
+          : ` bg-coral-red  text-white border-coral-red ${fullWidth && 'w-full'}`
       }`}
     >
       {label}
